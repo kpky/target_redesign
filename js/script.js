4d5a27e884c95916f7820db7c621694552419e6c
Karah Prokay <k.e.prokay@gmail.com>
2015-06-09 10:48:25 -0400

    reworked the featured content to display inline block instead of float left. Added hover states on category images.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-<script type="text/javascript">
-2
-$(function() {
-3
-$('.hover').smartmenus({
-4
-subMenusSubOffsetX: 1,
-5
-subMenusSubOffsetY: -8
-6
-});
-7
-});
-8
-</script>
+// <script type="text/javascript">
+// 2
+// $(function() {
+// 3
+// $('.hover').smartmenus({
+// 4
+// subMenusSubOffsetX: 1,
+// 5
+// subMenusSubOffsetY: -8
+// 6
+// });
+// 7
+// });
+// 8
+// </script>
+
+// mouseenter mouseleave
+
+
+// (function(window, undefined){
+// 	$(function(){
+
+// 		var $menu_button = $( "main_nav_container"),
+// 			$nav = $( ".top_nav"),
+// 		 	$lev2_nav = $("lev2_nav");
+
+// 		 	$nav.hide();
+// 		 	$lev2_nav.hide();
+
+// 		 .menu_button.on("mouseenter", )
+// 	})
+// })
